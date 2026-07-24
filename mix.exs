e342c1c90d060cfb8da153c6aa6ae25a13fe035c
John Davenport <johns10@gmail.com>
2026-07-23 21:14:56 -0400

    Release v0.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MarketMySpec.MixProject do
   def project do
     [
       app: :market_my_spec,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
